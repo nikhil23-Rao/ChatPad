2c5359ae5ca8de9bf2cc5973f194c5da7b39b37b
Nikhil <nikhil23.rao@gmail.com>
2021-04-29 14:32:25 -0700

    center register text

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -37,7 +37,7 @@ const Register: React.FC<RegisterProps> = ({ myproviders }: RegisterProps) => {
         </nav>
       </header>
 
-      <div className="container">
+      <div className="container text-center">
         <div className="row py-5 mt-4 align-items-center">
           <div className="col-md-5 pr-lg-5 mb-5 mb-md-0">
             <img
